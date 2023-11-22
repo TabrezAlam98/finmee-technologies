bee43fb20ef631ee523b8915149c285752358867
Tabrez Alam <97460388+TabrezAlam98@users.noreply.github.com>
2023-11-22 12:24:39 +0530

    creatd banner

--- ntwist/src/App.js
+++ ntwist/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
+
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
+    <div>
+      <Navbar />
+      <Banner />
     </div>
   );
 }
---
+++ ntwist/src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <img src="https://www.microcybers.com/static/upload/image/20220518/1652861604486220.jpg" />
+      <h1>Data powered-solutions for industrial Excellence</h1>
+      <button>Read More</button>
+    </div>
+  );
+};
+
+export default Banner;
